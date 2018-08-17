dbc0e13a56c536f59eb658b46d6717b972629907
DevHyung <khuphj@gmail.com>
2018-08-17 19:34:24 +0900

    Modify 유저별 대화를 다르게

--- chatbot_server.py
+++ chatbot_server.py
@@ -5,8 +5,7 @@ from flask import json
 from googletrans import Translator
 import Util
 #=== CONFIG
-IS_TALK_MODE = False
-
+USER_DICT = {} #User Queue  역할 
 
 #====================
 translator = Translator()
@@ -20,25 +19,34 @@ def keyboard():
 
 @app.route("/message", methods=["POST"])
 def message():
-    global IS_TALK_MODE
+    global USER_DICT
     data = json.loads(request.data)
     content = data["content"]
+
+    if data['user_key'] not in USER_DICT.keys():
+        USER_DICT[data['user_key']] = {'IS_TALK_MODE':False}
+
+    print(data,USER_DICT)
+    ###
+    if content == '아기랑 대화하기' and not USER_DICT[data['user_key']]['IS_TALK_MODE']:#대화모드를 맨처음 클릭하셨을때
+        USER_DICT[data['user_key']]['IS_TALK_MODE'] = True
+        response = Util.return_res_by_code(1)
+    elif content == '엄마가' and USER_DICT[data['user_key']]['IS_TALK_MODE']:#대화모드에서 종료
+        USER_DICT[data['user_key']]['IS_TALK_MODE'] = False
+        response = Util.return_res_by_code(2)
+
+    elif  USER_DICT[data['user_key']]['IS_TALK_MODE']:#대화 모드 중일 때
+        response = Util.return_res_by_code(-1)
+        print(content)
+
+    else: # 기본 FAQ 모드일때
+        response = Util.return_res_by_code(0)
+
+    #translated = translator.translate(content, dest="en", src="ko")
+    #result = translated.text
     ###
 
-    translated = translator.translate(content, dest="en", src="ko")
-    result = translated.text
-    ###
-    text = result
-    if content == '아기랑 대화하기' and not IS_TALK_MODE:#대화모드를 맨처음 클릭하셨을때
-        IS_TALK_MODE = True
-        response = Util.return_res_by_code(1)
-    elif IS_TALK_MODE:#대화 모드 중일 때
-        response = Util.return_res_by_code(2)
-    elif content == '대화종료' and IS_TALK_MODE:#대화모드에서 종료
-        IS_TALK_MODE = False
-        response = Util.return_res_by_code(0)
-    else:
-        response = Util.return_res_by_code(0)
+
 
     response = json.dumps(response, ensure_ascii=False)
     return response
