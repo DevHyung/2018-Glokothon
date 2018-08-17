0090145be5254f263382e22fd09698f08a53c8fa
DevHyung <khuphj@gmail.com>
2018-08-17 17:28:18 +0900

    Iter 잠시옮기기

---
+++ chatbot_server.py
@@ -0,0 +1,43 @@
+from flask import Flask
+from flask import request
+from flask import jsonify
+from flask import json
+from googletrans import Translator
+translator = Translator()
+app = Flask(__name__)
+
+
+@app.route("/keyboard")
+def keyboard():
+    return jsonify(type="text")
+
+
+@app.route("/message", methods=["POST"])
+def message():
+
+    data = json.loads(request.data)
+
+
+    print(request.headers)
+    # request.headers['X-Forwarded-For'] = '110.76.143.234'
+    print(request.data)
+    content = data["content"]
+    ###
+
+    translated = translator.translate(content, dest="en", src="ko")
+    result = translated.text
+    ###
+    text = result
+    response = {
+        "message": {
+            "text": text
+        }
+    }
+
+    response = json.dumps(response, ensure_ascii=False)
+    return response
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8888)
+    #./ngrok http 8888 --region ap
